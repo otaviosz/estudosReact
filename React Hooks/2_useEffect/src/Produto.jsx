a50d1a4ced449bb1e15515795407b9389f439ee8
Otávio <otaviodiascc@gmail.com>
2024-08-21 08:58:30 -0300

    useEffect

---
+++ React Hooks/2_useEffect/src/Produto.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Produto = () => {
+
+  React.useEffect(() => {
+    function handleScroll(e) {
+      console.log(e)
+    }
+  
+    window.addEventListener('scroll', handleScroll)  
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <div style={{height: "200vh"}}>
+      <p>Meu produto</p>
+    </div>
+  )
+}
+
+export default Produto
